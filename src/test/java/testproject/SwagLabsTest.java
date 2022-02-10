2e434ad5d8e04ffd6f3e8a9f318510221df8d915
JoseRomeroRamirez <77643339+JoseRomeroRamirez@users.noreply.github.com>
2022-02-09 22:47:56 -0600

    Update - Jose Romero

--- src/test/java/testproject/SwagLabsTest.java
+++ src/test/java/testproject/SwagLabsTest.java
@@ -1,13 +1,22 @@
 package testproject;
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 import week6.actions.Login;
 import week6.base.TestUtilities;
 
 public class SwagLabsTest  extends TestUtilities {
-    @Test
-    @Parameters({"user","pass"})
+    @DataProvider(name="user-data")
+    Object[][] userData(){
+        return new Object[][] {
+                {"standard_user","secret_sauce"},
+                {"locked_out_user", "secret_sauce"},
+                {"problem_user", "secret_sauce"},
+                {"performance_glitch_user","secret_sauce"}
+        };
+    }
+    @Test(dataProvider = "user-data")
     public void LoginTest(String user, String pass){
         Login Login = new Login(driver, log);
         log.info("Login in to the E-commerce");
