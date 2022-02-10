a41e699ddf0c291dc9f82a2675865e3ec3532a3d
JoseRomeroRamirez <77643339+JoseRomeroRamirez@users.noreply.github.com>
2022-02-09 22:10:25 -0600

    Update - Jose Romero

--- src/test/java/testproject/SwagLabsTest.java
+++ src/test/java/testproject/SwagLabsTest.java
@@ -8,7 +8,7 @@ import week6.base.TestUtilities;
 public class SwagLabsTest  extends TestUtilities {
     @Test
     @Parameters({"user","pass"})
-    public void Login(String user, String pass){
+    public void LoginTest(String user, String pass){
         Login Login = new Login(driver, log);
         log.info("Login in to the E-commerce");
         Login.execute(user, pass);
