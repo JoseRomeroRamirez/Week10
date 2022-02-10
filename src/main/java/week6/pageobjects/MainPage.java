1c5d0e902726e084c039cda0f1a33d13fb306d5b
JoseRomeroRamirez <77643339+JoseRomeroRamirez@users.noreply.github.com>
2022-02-10 16:35:32 -0600

    Update - Jose Romero

--- src/main/java/week6/pageobjects/MainPage.java
+++ src/main/java/week6/pageobjects/MainPage.java
@@ -18,13 +18,7 @@ public class MainPage extends BasePageObject{
         List<WebElement> addButton = findElements(addToCartButtonLocator);
         for (int j=0; j < addButton.size(); j++) {
             addButton.get(j).click();
-            System.out.println((j+1)+"    "+getText(cartCounterLocator));
             AssertEqual(String.valueOf(j+1),getText(cartCounterLocator),"El Contador de agregar al carrito fallo");
-        }
-        try {
-            Thread.sleep(10000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
         }
     }
 }
